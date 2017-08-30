c174dc5ac1e6f22cef139242e07862b932d41470
jamesrhee91 <jamesshrhee@gmail.com>
2017-08-30 19:54:01 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,36 @@
-// Code MovieReviews Here
+import React from 'react'
+
+
+// const Review = ({ title }) => {
+//   return (
+//     <div className="review">
+//       <h1>{title}</h1>
+//     </div>
+//   )
+// }
+//
+// const MovieReviews = ({ reviews }) => {
+//   return (
+//     <div className="review-list">
+//       {reviews.map((review, index) => <Review title={review.display_title} key={index}/>)}
+//     </div>
+//   )
+// }
+
+const MovieReviews = ({ reviews }) => {
+  return (
+    <div className="review-list">
+      {reviews.map((review, index) => {
+        return (
+          <div className="review" key={index}>
+            <h1>{review.display_title}</h1>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+MovieReviews.defaultProps = { reviews: [] }
+
+export default MovieReviews
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,30 @@
-// Code SearchableMovieReviewsContainer Here
+import React from 'react'
+import 'isomorphic-fetch';
+import MovieReviews from './MovieReviews'
+
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      reviews: [],
+      searchTerm: ''
+    }
+  }
+
+  componentWillMount() {
+    fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.searchTerm}&api-key=387a337eb88c4aba80d745832b467409`).then(res => res.json()).then(review => this.setState({ reviews: review.results }))
+  }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <MovieReviews reviews={this.state.reviews}/>
+      </div>
+    )
+  }
+
+}
+
+export default SearchableMovieReviewsContainer
